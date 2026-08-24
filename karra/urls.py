88c22d841520e2589380cf3c097c51a03832167f
umurbek <mamaevakim4@gmail.com>
2026-08-24 15:31:19 +0500

    Add Karra template frontend

--- karra/urls.py
+++ karra/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import (
+from karra.views import (
     RegisterAPIView,
     LoginAPIView,
     LogoutAPIView,
